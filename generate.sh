ae2bed7d131edbc91ba22c123369a95e06dce89e
David Kerkeslager <kerkeslager@gmail.com>
2018-12-07 12:52:32 -0500

    More setup stuff

---
+++ generate.sh
@@ -0,0 +1,4 @@
+#!/usr/bin/env bash
+
+yes | rm -r site
+cp -r site_source site
--- setup/run-on-server.sh
+++ setup/run-on-server.sh
@@ -1,5 +1,7 @@
 #!/usr/bin/env bash
 
+pushd ~/site2
+
 sudo update-alternatives --config editor
 
 sudo apt install nginx
@@ -7,9 +9,23 @@ sudo ufw allow 'Nginx Full'
 
 sudo mkdir -p /var/www/kerkeslager.com/html
 sudo chown -R $USER:$USER /var/www/kerkeslager.com/html
-sudo cp ~/site2/site/index.html /var/www/example.com/html/
 
-sudo cp ~/site2/nginx_configuration /etc/nginx/sites-available/kerkeslager.com
+./generate.sh
+sudo cp -r site/* /var/www/example.com/html/
+
+sudo cp nginx_configuration /etc/nginx/sites-available/kerkeslager.com
 sudo ln -s /etc/nginx/sites-available/kerkeslager.com /etc/nginx/sites-enabled/
 
+echo 'deb http://deb.debian.org/debian stretch-backports main contrib non-free' | sudo tee -a /etc/apt/sources.list
+echo 'deb-src http://deb.debian.org/debian stretch-backports main contrib non-free' | sudo tee -a /etc/apt/sources.list
+
+sudo apt update
+sudo apt install python-certbot-nginx -t stretch-backports
+
 sudo systemctl reload nginx
+
+sudo certbot --nginx -d kerkeslager.com -d www.kerkeslager.com
+
+sudo systemctl reload nginx
+
+popd
