dcf9b6bdfa8d83623e7955be2870e984bd5473ac
David Kerkeslager <kerkeslager@gmail.com>
2018-12-07 12:32:26 -0500

    Begin setup

--- setup/run-on-server.sh
+++ setup/run-on-server.sh
@@ -1,3 +1,15 @@
 #!/usr/bin/env bash
 
-echo "Run this"
+sudo update-alternatives --config editor
+
+sudo apt install nginx
+sudo ufw allow 'Nginx Full'
+
+sudo mkdir -p /var/www/kerkeslager.com/html
+sudo chown -R $USER:$USER /var/www/kerkeslager.com/html
+sudo cp ~/site2/site/index.html /var/www/example.com/html/
+
+sudo cp ~/site2/nginx_configuration /etc/nginx/sites-available/kerkeslager.com
+sudo ln -s /etc/nginx/sites-available/kerkeslager.com /etc/nginx/sites-enabled/
+
+sudo systemctl reload nginx
