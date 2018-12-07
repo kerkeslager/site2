57122ca5be5c2fdca91d61a9350a1ec7ab34de53
David Kerkeslager <kerkeslager@gmail.com>
2018-12-07 12:00:17 -0500

    Clone using HTTPS

--- setup/run.sh
+++ setup/run.sh
@@ -4,5 +4,5 @@
 # $2 is the username to provision with
 
 ssh -i ~/.ssh/$2 root@$1 "adduser $2 && usermod -aG sudo $2 && apt update && apt install ufw && ufw allow OpenSSH && cp -r ~/.ssh /home/$2 && chown -R $2:$2 /home/$2/.ssh"
-ssh $2@$1 'git clone git@github.com:kerkeslager/site2.git && ./site2/setup/run-on-server.sh'
+ssh $2@$1 'sudo apt install git && git clone https://github.com/kerkeslager/site2.git && ./site2/setup/run-on-server.sh'
 
