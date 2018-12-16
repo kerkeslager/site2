8345b74c3c3edd0c4696fc775594d4076d391e8f
David Kerkeslager <kerkeslager@gmail.com>
2018-12-16 04:22:04 -0500

    A bit more styling, and added a bunch of climbs to the climbing page

--- haunt.scm
+++ haunt.scm
@@ -77,54 +77,103 @@
                               "CC BY-SA 4.0"))
                      (span "Code: "
                            (a (@ (rel "license") (href "https://www.gnu.org/licenses/gpl.html"))
-                              "GPL 3.0"))))
-          )))))
+                              "GPL 3.0")))))))))
 
 (define (haunt-post-template post)
-  `((h2 ,(post-ref post 'title))
-    (h3 "by " ,(post-ref post 'author)
-        " — " ,(date->string* (post-date post)))
-    (div ,(post-sxml post))))
+  `(article (h1 ,(post-ref post 'title))
+						(div (@ (class "date")) ,(date->string (post-date post) "~Y ~m ~d"))
+						(div ,(post-sxml post))))
 
 (define (haunt-collection-template site title posts prefix)
-  (define (post-uri post)
-    (string-append "/" (or prefix "")
-                   (site-post-slug site post) ".html"))
+	(define (post-uri post)
+		(string-append "/" (or prefix "")
+									 (site-post-slug site post) ".html"))
 
-  `((h2 "Blog")
-    (ul ,@(map (lambda (post)
-                 `(li
-                    (a (@ (href ,(post-uri post)))
-                       ,(post-ref post 'title)
-                       " — "
-                       ,(date->string* (post-date post)))))
-               (posts/reverse-chronological posts)))))
+	`((h1 "Blog")
+    (a (@ (href "/feed.xml")
+          (class "rss"))
+       ,(embed-svg "feather/rss.svg"))
+		(ul (@ (class "blog-index"))
+        ,@(map (lambda (post)
+								 `(li
+										(a (@ (href ,(post-uri post)))
+											 ,(date->string (post-date post) "~Y ~m ~d")
+											 " — "
+											 ,(post-ref post 'title))))
+							 (posts/reverse-chronological posts)))))
 
 (define haunt-theme
-  (theme #:name "Haunt"
-         #:layout haunt-layout
-         #:post-template haunt-post-template
-         #:collection-template haunt-collection-template
-         ))
+	(theme #:name "Haunt"
+				 #:layout haunt-layout
+				 #:post-template haunt-post-template
+				 #:collection-template haunt-collection-template
+				 ))
 
 (define (index-page site posts)
-  (make-page "index.html"
-             (with-layout haunt-theme site "Home" "Homepage body content")
-             sxml->html))
+	(make-page "index.html"
+						 (with-layout haunt-theme site "Home"
+                          "My name is David and this is my new website. Check it out.")
+						 sxml->html))
+
+(define redpoints
+  '(ul (li "5.8 Psycho Crack Right")
+       (li "5.7 Reach Around")
+       (li "5.7 Kling-on")
+       (li "5.7(5.5R) The Brat")
+       (li "5.6 Laurel")))
+
+(define onsights
+  '(ul (li "5.6(PG13) Moonlight")
+       (li "5.6 High Exposure")
+       (li "5.6 Baby")
+       (li "5.7 First Day")
+       (li "5.6 Sundance")
+       (li "5.6 Genuflect")
+       (li "5.6(PG13) Eyesore")
+       (li "5.5 Ursula")))
+
+(define other-leads
+  '(ul (li "5.6 Cat in the Hat")
+       (li "5.9 Big Bad Wolf")
+       (li "5.6 Madame Grunnebaum's Wulst")
+       (li "5.6 Shockley's Ceiling")
+       (li "5.8 Bolt Line")
+       (li "5.8(5.6PG13) City Lights")
+       (li "5.4(R) Beginner's Delight")))
+
+(define boulders
+  '(ul (li "V2 M4")
+       (li "V1(PG13) Suzie A")))
+
+(define selected-leads
+  `(section (h2 "Selected Leads")
+            (p "This is an abridged list of the things I've climbed. I included climbs either because I'm proud of having lead them, or because they were memorable or enjoyable.")
+            (section (h3 "Redpoints")
+                     (p "These are climbs that I spent a significant amount of time working on before I was able to lead climb them without falling.")
+                     ,redpoints)
+            (section (h3 "Onsights")
+                     (p "These are climbs that I climbed on the first try without beta and without weighting the rope.")
+                     ,onsights)
+            (section (h3 "Other leads")
+                     (p "These are leads that I didn't redpoint or onsight, but are worth mentioning.")
+                     ,other-leads)
+            (section (h3 "Boulder problems")
+                     (p "I'm not much of a boulderer, but here are a few boulder problems I've done.")
+                     ,boulders)))
+
 
 (define (climbing-index-page site posts)
   (make-page "climbing/index.html"
-             (with-layout haunt-theme site "Climbing" "Climbing body content")
+             (with-layout haunt-theme site "Climbing" selected-leads)
              sxml->html))
 
 (define %collections
-  `(("Home" "index.html" ,posts/reverse-chronological)))
+  `(("Blog" "index.html" ,posts/reverse-chronological)))
 
 (site #:title "David Kerkeslager's Website"
       #:domain "kerkeslager.com"
       #:default-metadata
-      '((author . "David Kerkeslager")
-        (email  . "kerkeslager@riseup.net"))
+      '((author . "David Kerkeslager"))
       #:readers (list sxml-reader html-reader)
       #:builders (list index-page
                        climbing-index-page
