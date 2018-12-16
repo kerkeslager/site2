d61322292692fce1e182fc5eac9fab6251015157
David Kerkeslager <kerkeslager@gmail.com>
2018-12-15 21:25:55 -0500

    Fix copyright symbol, test post changes deployment process

--- haunt.scm
+++ haunt.scm
@@ -41,7 +41,7 @@
           (a (@ (rel "license") (href "http://creativecommons.org/licenses/by-sa/4.0/"))
              (img (@ (alt "Creative Commons License") (style "border-width:0") (src "/img/88x31.png"))))
           (br)
-          "All content is &copy; 2018 by David Kerkeslager and released under the"
+          "All content is © 2018 by David Kerkeslager and released under the"
           (a (@ (rel "license") (href "http://creativecommons.org/licenses/by-sa/4.0/")) "Creative Commons Attribution-ShareAlike 4.0 International License")
           " unless otherwise specified.")))))
 
